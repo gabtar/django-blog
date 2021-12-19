6f03aad0553b1ddf8e49eae999756747ea508d76
Gabriel Tarifa <gabrieltarifa@gmail.com>
2021-12-19 14:53:33 -0300

    Uso de viewsets y documentación de la api
    Hasta acá, fue siguiendo el modelo del libro Django For Apis.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,5 @@
 from posts.models import Post
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
@@ -6,4 +7,11 @@ class PostSerializer(serializers.ModelSerializer):
     """ Serializador para el modelo de Post """
     class Meta:
         model = Post
-        fields = ('id', 'author', 'title', 'body', 'created_at')
+        fields = ('id', 'author', 'title', 'body', 'created_at',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Serializador para el modelo de usuario base de django """
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,18 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 from posts.permissions import IsAuthorOrReadOnly
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    """ Endpoint para listar y/o crear posts (sólo GET y POST) """
-    serializer_class = PostSerializer
-    queryset = Post.objects.all()
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    """ Endpoint para obtener, actualizar o eliminar un post (GET, PUT, DELETE) """
+class PostViewSet(viewsets.ModelViewSet):
+    """ Viewset para manejar los endpoint del modelo de post """
     permission_classes = (IsAuthorOrReadOnly,)
-    serializer_class = PostSerializer
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ Viewset para manejar los endpoint del modelo de usuario """
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
